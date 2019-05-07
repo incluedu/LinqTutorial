d2779b798a2f65cb96e04c4111fde3b94fad54a6
Patric Hollenstein <patric.hollenstein@ltw.at>
2019-05-07 10:11:03 +0200

    something ....

---
+++ c006_SortData/User.cs
@@ -0,0 +1,9 @@
+namespace c006_SortData
+{
+    partial class User
+    {
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public int Age { get; set; }
+    }
+}
